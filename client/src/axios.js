3cd76c2dfb13c4df254402a8997045b34f6898eb
= <=>
2026-07-14 23:20:29 +0530

    Fixed API routes

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://corporate-company.onrender.com",
+  baseURL: "https://corporate-company.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
